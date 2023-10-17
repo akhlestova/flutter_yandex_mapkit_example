d19e7c2abc88214a2c1c48b04f3d5c8601dfc541
Anna Akhlestova <a.akhlestova@gmail.com>
2023-10-17 22:02:02 +0300

    add driving routes

--- lib/presentation/map_screen.dart
+++ lib/presentation/map_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
@@ -25,9 +27,19 @@ class _MapScreenState extends State<MapScreen> {
   /// Список точек на карте, по которым строится выделенная область
   List<Point>? _polygonPointsList;
 
+  /// Список точек на карте, по которым строится автомобильный маршрут
+  List<Point> _drivingPointsList = [];
+
+  /// Результаты поиска маршрутов на карте
+  DrivingResultWithSession? _drivingResultWithSession;
+
+  /// Список объектов линий на карте, которые отображают маршруты
+  List<MapObject> _drivingMapLines = [];
+
   @override
   void dispose() {
     _mapController.dispose();
+    _drivingResultWithSession?.session.close();
     super.dispose();
   }
 
@@ -64,11 +76,35 @@ class _MapScreenState extends State<MapScreen> {
             _mapZoom = cameraPosition.zoom;
           });
         },
+        onMapLongTap: (argument) {
+          setState(() {
+            // добавляем точку маршрута на карте, если еще не выбраны две точки
+            if (_drivingPointsList.length < 2) {
+              _drivingPointsList.add(argument);
+            } else {
+              _drivingPointsList = [];
+            }
+
+            // когда выбраны точки начала и конца,
+            // получаем данные предложенных маршрутов
+            if (_drivingPointsList.length == 2) {
+              _drivingResultWithSession = _getDrivingResultWithSession(
+                startPoint: _drivingPointsList.first,
+                endPoint: _drivingPointsList.last,
+              );
+             
+            }
+          });
+          
+          _buildRoutes();
+        },
         mapObjects: [
           _getClusterizedCollection(
             placemarks: _getPlacemarkObjects(context),
           ),
           _getPolygonMapObject(context, points: _polygonPointsList ?? []),
+          ..._getDrivingPlacemarks(context, drivingPoints: _drivingPointsList),
+          ..._drivingMapLines,
         ],
         onUserLocationAdded: (view) async {
           // получаем местоположение пользователя
@@ -153,6 +189,27 @@ class _MapScreenState extends State<MapScreen> {
       });
     }
   }
+
+  /// Метод построения маршрутов линиями на карте
+  /// Получает список возможных маршрутов и добавляет их линиями на карту
+  Future<void> _buildRoutes() async {
+    final drivingResult = await _drivingResultWithSession?.result;
+
+    setState(() {
+      for (var element in drivingResult?.routes ?? []) {
+        _drivingMapLines.add(
+          PolylineMapObject(
+            mapId: MapObjectId('route $element'),
+            polyline: Polyline(points: element.geometry),
+            strokeColor:
+                // генерируем случайный цвет для каждого маршрута
+                Colors.primaries[Random().nextInt(Colors.primaries.length)],
+            strokeWidth: 3,
+          ),
+        );
+      }
+    });
+  }
 }
 
 /// Метод для генерации точек на карте
@@ -193,6 +250,30 @@ List<PlacemarkMapObject> _getPlacemarkObjects(BuildContext context) {
       .toList();
 }
 
+/// Метод для генерации точек начала и конца маршрута
+List<PlacemarkMapObject> _getDrivingPlacemarks(
+  BuildContext context, {
+  required List<Point> drivingPoints,
+}) {
+  return drivingPoints
+      .map(
+        (point) => PlacemarkMapObject(
+          mapId: MapObjectId('MapObject $point'),
+          point: Point(latitude: point.latitude, longitude: point.longitude),
+          opacity: 1,
+          icon: PlacemarkIcon.single(
+            PlacemarkIconStyle(
+              image: BitmapDescriptor.fromAssetImage(
+                'assets/icons/car_point.png',
+              ),
+              scale: 2,
+            ),
+          ),
+        ),
+      )
+      .toList();
+}
+
 /// Метод для генерации объекта выделенной зоны на карте
 PolygonMapObject _getPolygonMapObject(
   BuildContext context, {
@@ -222,6 +303,33 @@ PolygonMapObject _getPolygonMapObject(
       ),
     ),
   );
+}
+
+/// Метод для получения маршрутов проезда от точки начала к точке конца
+DrivingResultWithSession _getDrivingResultWithSession({
+  required Point startPoint,
+  required Point endPoint,
+}) {
+  var drivingResultWithSession = YandexDriving.requestRoutes(
+    points: [
+      RequestPoint(
+        point: startPoint,
+        requestPointType: RequestPointType.wayPoint, // точка начала маршрута
+      ),
+      RequestPoint(
+        point: endPoint,
+        requestPointType: RequestPointType.wayPoint, // точка конца маршрута
+      ),
+    ],
+    drivingOptions: const DrivingOptions(
+      initialAzimuth: 0,
+      routesCount: 5,
+      avoidTolls: true,
+      avoidPoorConditions: true,
+    ),
+  );
+
+  return drivingResultWithSession;
 }
 
 /// Содержимое модального окна с информацией о точке на карте
